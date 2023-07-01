38b43d54ac162b5cd7facc96c6fb8872985601a7
Rakesh Kumar Jangid <77071695+rakesh08061994@users.noreply.github.com>
2023-07-01 13:35:39 +0530

    Create kube-setup.sh
    Before using this script make sure -
    Instances OS should be UBUNTU
    Instances infrastructure should be atleast T2.Medium with 20 GB storage each. 
    Check Inbound rule
    All instances have there proper hostname and ping properly.
    Check all instances hostname entry in /etc/hosts
    Selinux and firewall should be disable and stopped

---
+++ kube-setup.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl restart docker
+sudo systemctl enable docker
+# -------------------------------- Docker Installation complete ---------------------------------------
+git clone https://github.com/Mirantis/cri-dockerd.git
+sudo wget https://storage.googleapis.com/golang/getgo/installer_linux
+chmod +x ./installer_linux
+./installer_linux
+source ~/.bash_profile
+cd cri-dockerd
+mkdir bin
+go build -o bin/cri-dockerd
+mkdir -p /usr/local/bin
+sudo install -o root -g root -m 0755 bin/cri-dockerd /usr/local/bin/cri-dockerd
+cp -a packaging/systemd/* /etc/systemd/system
+sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
+systemctl daemon-reload
+systemctl enable cri-docker.service
+systemctl enable --now cri-docker.socket
+apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+systemctl start kubelet
+systemctl start cri-docker
+mkdir -p /etc/docker/
+touch /etc/docker/daemon.json
+echo '{
+  "exec-opts": ["native.cgroupdriver=systemd"]
+}' > /etc/docker/daemon.json
+systemctl restart docker 
+systemctl restart kubelet
+echo "##########################################################################
+
+
+
+ After this you have to require to run below command on specific node:
+
+# On Master Node: kubeadm init --cri-socket unix:///var/run/cri-dockerd.sock
+
+# On Slave Node: Master_Node_Token  --cri-socket unix:///var/run/cri-dockerd.sock
+
+###########################################################################"
+echo "##########################################################################
+
+Follow this steps and apply on master node for Installing Calico networking 
+https://docs.tigera.io/calico/latest/getting-started/kubernetes/self-managed-onprem/onpremises#install-calico-with-kubernetes-api-datastore-50-nodes-or-less > Follow these steps
+---- OR -----
+Run these command on Master Node :-
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/calico.yaml -O
+kubectl apply -f calico.yaml
+------ Wait five minutes----
+kubectl get nodes -o wide
+###########################################################################"
+
+
